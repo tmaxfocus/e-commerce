3ff715088681a94ea04b404f44e00a97617710ee
tobi adeogun <tobiadeomax@outlook.com>
2024-06-15 10:14:48 +0100

    intial push

---
+++ src/main/java/com/tmaxnoda/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.tmaxnoda.ecommerce.config;
+
+import com.tmaxnoda.ecommerce.entity.Product;
+import com.tmaxnoda.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //disable Http methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable((theUnsupportedActions)));
+
+        //disable Http methods for Product-Category: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable((theUnsupportedActions)));
+    }
+}
